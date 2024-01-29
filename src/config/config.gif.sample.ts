1efe30f76f31dc75c4af5084eb3770d8d0f8fd53
Nathan Brady <nbrady@techempower.com>
2024-01-29 08:51:01 -0800

    fix config.gif.sample.ts with proper urls

--- src/config/config.gif.sample.ts
+++ src/config/config.gif.sample.ts
@@ -2,9 +2,9 @@ const userGiftConfig: UserGifConfig = {
   // If the !gif <phrase> matches one of these keys, use that gif URL instead
   // Use lowercase for the keys
   matches: {
-    kaz: 'https://lichess.org/blunderbot/gifs/kaz.gif',
-    party: 'https://lichess.org/blunderbot/gifs/party.gif',
-    ambulance: 'https://lichess.org/blunderbot/gifs/ambulance.gif'
+    kaz: 'https://localhost/gifs/kaz.gif',
+    party: 'https://localhost/gifs/party.gif',
+    ambulance: 'https://localhost/gifs/ambulance.gif'
   },
   // If the !gif <phrase> doesn't match any of the keys above, use this URL instead
   notFound: 'https://localhost/gifs/404.gif'
